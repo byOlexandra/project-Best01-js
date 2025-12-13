9174d9703dad39069d31db483ee633a55498f119
devolexa <starovatovasasha@gmail.com>
2025-12-13 07:48:11 +0200

    add 4 libraries, add css styles

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+import { raty } from 'raty-js';
+
+import Swal from 'sweetalert2';
