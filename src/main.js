02540e21d5d11b9c07db4862347472e66e2e693d
devolexa <starovatovasasha@gmail.com>
2025-12-15 13:08:19 +0200

    add import in main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-import Swal from 'sweetalert2';
-
 import { initAccordion } from './js/FAQ-section.js';
-
-// Завантаження бібліотеки у FAQ section
 initAccordion();
+
+import { openModal, closeModal } from './js/order-modal.js';
+openModal();
+closeModal();
