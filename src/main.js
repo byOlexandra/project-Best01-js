8f4a8ee1b997a4f967de806a32cc7b13e2636ad9
devolexa <starovatovasasha@gmail.com>
2025-12-15 13:28:37 +0200

    del useles import

--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-import Swal from 'sweetalert2';
-
 import { initAccordion } from './js/FAQ-section.js';
-
-// Завантаження бібліотеки у FAQ section
 initAccordion();
