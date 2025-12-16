a5727268c783d56802b484025212dcfd52511dfb
Maksim Voloshchuk <maksimvolosuk684@gmail.com>
2025-12-17 00:04:17 +0100

    added new section new main 8

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { initAccordion } from './js/FAQ-section.js';
 initAccordion();
 
-import { openModal, closeModal } from './js/order-modal.js';
-openModal();
-closeModal();
-
 import { initWhySlider } from './js/about-us.js';
 
 initWhySlider();
