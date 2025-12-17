303c861dd46309cba1f98b6a14337d07872d5f6b
devolexa <starovatovasasha@gmail.com>
2025-12-17 15:57:37 +0200

    change main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import { initAccordion } from './js/FAQ-section.js';
-initAccordion();
-
 import { openModal, closeModal } from './js/order-modal.js';
-openModal();
-closeModal();
-
 import { initMobileMenu } from './js/mobile-menu.js';
-initMobileMenu();
 import { initSwiper } from './js/about-us.js';
-initSwiper();
+import { addCategories, getPets, renderPets } from './js/pets-list.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initAccordion();
+    openModal();
+    closeModal();
+    initMobileMenu();
+    initSwiper();
+
+    addCategories();
+    getPets('all', 1);
+
+    const API_URL = 'https://paw-hut.b.goit.study/api/animals';
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(data => renderPets(data.pets))
+        .catch(err => console.error(err));
+});
+
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
